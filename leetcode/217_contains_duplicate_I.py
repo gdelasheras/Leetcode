c1382f13aa9b48ccc609121cdc5c420285509a0c
Gonzalo de las Heras de Matías <gdelasherasdematias@gmail.com>
2025-01-31 17:49:26 +0100

    217

---
+++ leetcode/217_contains_duplicate_I.py
@@ -0,0 +1,26 @@
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+T: O(n) n = length of the array
+S: O(k) k = distinct numbers in the array
+"""
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        visited = set()
+        for num in nums:
+            if num in visited:
+                return True
+            visited.add(num)
+        return False
+
+
+def test_containsDuplicate():
+    s = Solution()
+    assert s.containsDuplicate([1, 2, 3, 4]) == False
+    assert s.containsDuplicate([1, 4, 3, 4]) == True
+    assert s.containsDuplicate([1]) == False
+    assert s.containsDuplicate([]) == False
